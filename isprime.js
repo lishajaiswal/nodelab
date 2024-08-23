22dee20fcd99d1b2c250e2a07c044d8057d85ba3
Lisha <lishajaiswal199@gmail.com>
2024-08-23 13:21:05 +0530

    checking prime

---
+++ isprime.js
@@ -0,0 +1,17 @@
+// Function to check prime number
+
+function checkPrime(num) {
+    let i, value = true;
+    for (i = 2; i <= num - 1; i++) {
+        if (num % i == 0) {
+            value = false;
+            break;
+        }
+    }
+    if (value == true)
+        console.log(num + " is prime");
+    else
+        console.log(num + " is not prime");
+}
+checkPrime(4);
+checkPrime(5);
